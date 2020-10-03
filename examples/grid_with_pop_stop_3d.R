9e00cfdbf356f46ebda78122b349a998bd7b5ea8
Egor Galkin <galkin@insilicomedicine.com>
2020-10-03 13:50:20 +0300

    Updated 3d script to allow for partial runs

--- examples/grid_with_pop_stop_3d.R
+++ examples/grid_with_pop_stop_3d.R
@@ -2,33 +2,35 @@ library(pacman)
 p_load(tidyverse, future, promises, listenv, fOptions)
 library(MathBioSim)
 
-result_dir = './24_8_2020_sims_3d/'
+result_dir = './1_10_2020_sims_3d/'
 dir.create(result_dir, showWarnings = FALSE)
 dir.create(paste0(result_dir,"pop/"), showWarnings = FALSE)
 
-plan(multiprocess)
+plan(multisession)
 
 
 n_dim=3
 
-grid_points <- expand.grid(comp_strength = seq(0.1,10,by=0.1),
+grid_points <- expand.grid(comp_strength = seq(1,10,by=0.1),
                            comp_width = 10^seq(-1,1,by=0.1))
 
 params_all <- grid_points%>%
   mutate(b=1,d=0,sm=1,sw=comp_width)%>%
   mutate(dd=comp_strength*(sw*sw*2*pi)^(n_dim/2))%>%
-  mutate(kernel_trim = 3, spline_precision = 1e-9, spline_nodes=1001L)%>%
+  mutate(kernel_trim = 3, spline_precision = 1e-9, spline_nodes=101L)%>%
   mutate(death_kernel_r=kernel_trim*sw,birth_kernel_r=kernel_trim*sm)%>%
   mutate(id=row_number(),seed=1234L)%>%
-  mutate(initial_population = 1e3L, population_limit = 1e6L)%>%
-  mutate(area_length_x=10*pmax(sm,sw),periodic=TRUE,cell_count_x=100L)%>%
+  mutate(initial_population = 1e3L, population_limit = 1e5L)%>%
+  mutate(area_length_x=10*pmax(sm,sw),periodic=TRUE,cell_count_x=10L)%>%
   mutate(n_samples = 100L)
 
 
 
 all_runs = listenv()
 
-for (i in params_all$id) {
+params_done <- list.files(paste0(result_dir,"pop/"))%>%str_remove('.csv')%>%as.numeric()
+
+for (i in sample(setdiff(params_all$id,params_done))) {
   params=params_all[i,]
   all_runs[[i]]%<-%
   {
@@ -71,18 +73,18 @@ for (i in params_all$id) {
     sim<-new(poisson_3d,sim_params)
     time<-numeric(params$n_samples)
     pop<-numeric(params$n_samples)
-    
+    pop_cap_reached<-numeric(params$n_samples)
     
     for(j in 1:params$n_samples){
       sim$run_events(sim$total_population)
       pop[j]=sim$total_population
       time[j]=sim$time
+      cap_reached=sim$pop_cap_reached
     }
     
-    pops<-data.frame(id=i,time=time,pop=pop)
+    pops<-data.frame(id=i,time=time,pop=pop,cap_reached=cap_reached)
     
     write_csv(pops,paste0(result_dir,"pop/",i,".csv"))
-    sim$pop_cap_reached
   }
 }
 
@@ -98,11 +100,11 @@ summary_results <-
   read_csv(paste0(result_dir,"pop.csv"))%>%
   group_by(id)%>%
   arrange(id,time)%>%
-  slice_tail(prop=0.9)%>%
-  summarise(average_pop=mean(pop))%>%
+  slice_tail(prop=0.8)%>%
+  summarise(average_pop=mean(pop),cap_reached=any(cap_reached))%>%
   left_join(params_all%>%select(id,area_length_x,comp_strength,comp_width))%>%
-  mutate(cap_reached = pop_cap_reached, N = average_pop / area_length_x^n_dim)%>%
-  select(id,comp_strength,comp_width,N,cap_reached,everything())
+  mutate(N = average_pop / area_length_x^n_dim)%>%
+  select(id,comp_strength,comp_width,N,everything()) 
 
 summary_results%>%write_csv(paste0(result_dir,"summary.csv"))
 

072f9f70189ad08f43f35f8302dad9f004b7bfad
Zelenkov Viktor <zelenkvik@gmail.com>
2021-02-23 14:10:07 +0300

    Fix modules names

--- src/poisson_n_species.cpp
+++ src/poisson_n_species.cpp
@@ -14,13 +14,13 @@ RCPP_MODULE(poisson_n_species_module) {
         .method("make_event", &Grid<1>::MakeEvent)
         .method("total_population", &Grid<1>::GetTotalPopulation);
         
-    class_<Grid<2>>("poisson_1d_n_species")
+    class_<Grid<2>>("poisson_2d_n_species")
         .constructor<List>()
         
         .method("make_event", &Grid<2>::MakeEvent)
         .method("total_population", &Grid<2>::GetTotalPopulation);
         
-    class_<Grid<3>>("poisson_1d_n_species")
+    class_<Grid<3>>("poisson_3d_n_species")
         .constructor<List>()
         
         .method("make_event", &Grid<3>::MakeEvent)

2a7ac3bde4ae761ef2e21c8c373ca0c18ae5392b
Fuuel <fueldanich@yandex.ru>
2021-12-11 04:30:38 +0300

    [up] added possibility of going along grids of biological parameters

--- examples/par_sim.r
+++ examples/par_sim.r
@@ -4,16 +4,16 @@ library(MathBioSim)
 
 initial_population = list()
 
-# Set up multiprocess parallel execution environment with availableCores() parallel processes
-plan(multiprocess(workers = availableCores()))
+# Set up multisession parallel execution environment with availableCores() parallel processes
+plan(multisession(workers = availableCores()))
 
 # Collects results from different processes
 sim_results = listenv()
 
-simulation <- function(area_length_x = 100, area_length_y = 100, area_length_z = 100, 
-                cell_count_x = 100, cell_count_y = 100, cell_count_z = 100, periodic = TRUE, 
+simulation <- function(area_length_x = 100, 
+                cell_count_x = 100, periodic = TRUE, 
                 b = 1, d = 0, dd = 0, seed = 1234L, 
-                initial_population_x = c(10), initial_population_y = c(10), initial_population_z = c(10),
+                initial_population_x = c(10),
                 death_r = 1, death_y, birth_ircdf_y, realtime_limit = 1e6, ndim = 1, epochs = 1000, 
                 calculate.pcf = FALSE, pcf_grid) {
   # Stores expression as a future, future gets send to different process
@@ -21,28 +21,51 @@ simulation <- function(area_length_x = 100, area_length_y = 100, area_length_z =
   # Different starting populations for example
   # Increase area length and runtime for better PCF results
   sim_results[[i]]%<-%{
-    sim<-initialize_simulator(area_length_x = area_length_x, area_length_y = area_length_y, 
-                                area_length_z = area_length_z, cell_count_x = cell_count_x,
-                                cell_count_y = cell_count_y, cell_count_z = cell_count_z, periodic = periodic,
+    sim<-initialize_simulator(area_length_x = area_length_x, 
+                                cell_count_x = cell_count_x,
+                                periodic = periodic,
                                 b = b, d = d, dd = dd, seed = seed, 
-                                initial_population_x = initial_population_x, initial_population_y = initial_population_y,
-                                initial_population_z = initial_population_z, death_r = death_r,
+                                initial_population_x = initial_population_x,
+                                death_r = death_r,
                                 death_y = death_y, birth_ircdf_y = birth_ircdf_y, realtime_limit = realtime_limit, ndim = ndim)
+    
+    res<-run_simulation(sim, epochs = epochs, calculate.pcf = calculate.pcf, pcf_grid = pcf_grid)
+    res[['initial_parameters']]<-data.frame(b=b,d=d,dd=dd)
     # Last expression is a return value of a future
-    run_simulation(sim, epochs = epochs, calculate.pcf = calculate.pcf, pcf_grid = pcf_grid)
+    res
   }
 }
 
-N_l = 1
-N_r = 10
-step = 1
 
-for (i in seq(N_l, N_r, step)) {
-  simulation(area_length_x = 10, dd = 0.01, initial_population_x = seq(0, 10, length.out = 100*i), death_r = 5, 
-      death_y = dnorm(seq(0, 5, length.out = 1001), sd = 1), 
-      birth_ircdf_y = qnorm(seq(0.5, 1-1e-6, length.out = 101), sd = 0.2), 
-      realtime_limit = 60, calculate.pcf = TRUE, 
-      pcf_grid = seq(0,5,length.out = 1001))
+i = 0L
+
+b_l = 1.0
+b_r = 1.1
+b_step = 0.1
+
+d_l = 0
+d_r = 0.1
+d_step = 0.1
+
+dd_l = 0.001
+dd_r = 0.0011
+dd_step = 0.0001
+
+for (b_value in seq(b_l, b_r, b_step)) {
+  for (d_value in seq(d_l, d_r, d_step)) {
+    for (dd_value in seq(dd_l, dd_r, dd_step)) {
+      i = i + 1L
+      simulation(area_length_x = 10,
+          b = b_value,
+          d = d_value,
+          dd = dd_value,
+          initial_population_x = seq(0, 10, length.out = 100), death_r = 5, 
+          death_y = dnorm(seq(0, 5, length.out = 1001), sd = 1), 
+          birth_ircdf_y = qnorm(seq(0.5, 1-1e-6, length.out = 101), sd = 0.2), 
+          realtime_limit = 60, calculate.pcf = TRUE, 
+          pcf_grid = seq(0,5,length.out = 1001))
+    }
+  }
 }
 
 
@@ -61,8 +84,8 @@ sim_results%>%
   purrr::map(~.x[['pcf']])%>%
   bind_rows(.id='ID')%>% 
   group_by(r)%>%
-  summarise(pcf=mean(pcf))%>% # Average across 10 runs
-  ggplot(aes(x=r,y=pcf))+
+  #summarise(pcf=mean(pcf))%>% # Average across 10 runs
+  ggplot(aes(x=r,y=pcf,color=ID))+
   geom_line()
 
 
@@ -71,6 +94,6 @@ sim_results%>%
   purrr::map(~.x[['K']])%>%
   bind_rows(.id='ID')%>% 
   group_by(r)%>%
-  summarise(K_iso=mean(K_iso))%>%
-  ggplot(aes(x=r,y=K_iso))+
+  #summarise(K_iso=mean(K_iso))%>%
+  ggplot(aes(x=r,y=K_iso,color=ID))+
   geom_line()
